2656b8330b9097a11e9a40e74503f002d1a7042d
avm1234567 <architmore303@gmail.com>
2026-06-04 21:28:19 +0530

    Added XOR_Cipher example

---
+++ Cryptography/XOR_Cipher/XORCipher_main.c
@@ -0,0 +1,49 @@
+#include "rcc.h"
+#include "gpio.h"
+#include "usart1.h"
+
+void XOR_Cipher(char *data, int key)
+{
+	while(*data)
+	{
+		if(*data != ' ')
+		{
+			*data = *data ^ key;
+		}
+		data++;
+	}
+}
+
+int main(void)
+{
+
+    USART1_Init();
+    char msg[50];
+    char cipherkey[10];
+    int Ckey;
+
+    while(1)
+    {   
+        USART1_SendString("Enter PlainText:");
+        USART1_ReceiveString(msg);
+        USART1_SendString("\r\n");
+        USART1_SendString("Enter Key:");
+        USART1_ReceiveString(cipherkey);
+        Ckey = USART1_StringToInt(cipherkey);
+        USART1_SendString("\r\n");
+        USART1_SendString(msg);
+        USART1_SendString("\r\n");
+        USART1_SendString(cipherkey);
+        USART1_SendString("\r\n");
+        XOR_Cipher(msg, Ckey);
+        USART1_SendString("CipherText:");
+        USART1_SendString(msg);
+        USART1_SendString("\r\n");
+        XOR_Cipher(msg, Ckey);
+        USART1_SendString("PlainText:");
+        USART1_SendString(msg);
+        USART1_SendString("\r\n");
+
+    }
+
+}
